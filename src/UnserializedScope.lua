2fc4da6c3b0ea2c0cca5159555ea10c040a21106
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-10-17 20:39:37 -0400

    Create scope that isn't serialized.

---
+++ src/UnserializedScope.lua
@@ -0,0 +1,34 @@
+--[[
+TheNexusAvenger
+
+Extension of the GenericScope that disables serialization.
+This is only intended for storing default setting.
+--]]
+--!strict
+
+local GenericScope = require(script.Parent:WaitForChild("GenericScope"))
+local Types = require(script.Parent:WaitForChild("Types"))
+
+local UnserializedScope = {}
+UnserializedScope.__index = UnserializedScope
+setmetatable(UnserializedScope, GenericScope)
+
+
+
+--[[
+Creates a generic scope.
+--]]
+function UnserializedScope.new(SerializationData: any?, ParentScope: Types.SettingsScope?): GenericScope.GenericScope
+    return (setmetatable(GenericScope.new(SerializationData, ParentScope), UnserializedScope) :: any) :: GenericScope.GenericScope
+end
+
+--[[
+Serializes the settings to be saved for later.
+--]]
+function UnserializedScope:Serialize(): any?
+    return nil
+end
+
+
+
+return (UnserializedScope :: any) :: GenericScope.GenericScope
---
+++ test/UnserializedScope.spec.lua
@@ -0,0 +1,51 @@
+--[[
+TheNexusAvenger
+
+Tests the UnserializedScope class.
+--]]
+--!strict
+
+local UnserializedScope = require(game:GetService("ReplicatedStorage").NexusScopedSettings.UnserializedScope)
+
+return function()
+    describe("An UnserializedScope instance", function()
+        it("should create from no serialization data.", function()
+            local Scope = UnserializedScope.new()
+            expect(Scope:Get("Setting1")).to.equal(nil)
+            expect(Scope:Get("Setting2")).to.equal(nil)
+            expect(Scope:Serialize()).to.equal(nil)
+
+            Scope:Set("Setting1", "Value1")
+            expect(Scope:Get("Setting1")).to.equal("Value1")
+            expect(Scope:Get("Setting2")).to.equal(nil)
+            expect(Scope:Serialize()).to.equal(nil)
+
+            Scope:Set("Setting1", "Value2")
+            expect(Scope:Get("Setting1")).to.equal("Value2")
+            expect(Scope:Get("Setting2")).to.equal(nil)
+            expect(Scope:Serialize()).to.equal(nil)
+
+            Scope:Set("Setting2", "Value3")
+            expect(Scope:Get("Setting1")).to.equal("Value2")
+            expect(Scope:Get("Setting2")).to.equal("Value3")
+            expect(Scope:Serialize()).to.equal(nil)
+        end)
+
+        it("should create from serialization data.", function()
+            local Scope = UnserializedScope.new({Setting1="Value1"})
+            expect(Scope:Get("Setting1")).to.equal("Value1")
+            expect(Scope:Get("Setting2")).to.equal(nil)
+            expect(Scope:Serialize()).to.equal(nil)
+
+            Scope:Set("Setting1", "Value2")
+            expect(Scope:Get("Setting1")).to.equal("Value2")
+            expect(Scope:Get("Setting2")).to.equal(nil)
+            expect(Scope:Serialize()).to.equal(nil)
+
+            Scope:Set("Setting2", "Value3")
+            expect(Scope:Get("Setting1")).to.equal("Value2")
+            expect(Scope:Get("Setting2")).to.equal("Value3")
+            expect(Scope:Serialize()).to.equal(nil)
+        end)
+    end)
+end
